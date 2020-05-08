dfcb9702bad28b7c7a6ba241baec56b2fd6d4c78
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-08 22:35:11 +0430

    LoginScreen add some decoration to image

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,12 +5,24 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(bottom: 80),
         child: Column(
           children: <Widget>[
-            Image(
-              image: AssetImage("assets/logo_round.png"),
-              width: 200,
-              height: 200,
+            Container(
+              margin: EdgeInsets.only(top: 80),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Color(0x4400F58D),
+                    blurRadius: 30,
+                    offset: Offset(10, 10),
+                    spreadRadius: 0),
+              ]),
+              child: Image(
+                image: AssetImage("assets/logo_round.png"),
+                width: 200,
+                height: 200,
+              ),
             )
           ],
         ),
