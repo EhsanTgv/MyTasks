b51c4494d1a28f2d82b66affd77b3cc41930ae9e
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-12 23:47:29 +0430

    PhoneSignInScreen implement design

--- lib/screens/phone_signin_screen.dart
+++ lib/screens/phone_signin_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:mytask/config/config.dart';
 
 class PhoneSignInScreen extends StatefulWidget {
   @override
@@ -6,10 +8,59 @@ class PhoneSignInScreen extends StatefulWidget {
 }
 
 class _PhoneSignInScreenState extends State<PhoneSignInScreen> {
+  PhoneNumber _phoneNumber;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Phone Sign In"),),
+      appBar: AppBar(
+        title: Text("Phone Sign In"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(top: 20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: InternationalPhoneNumberInput(
+                  onInputChanged: (phoneNumberText) {
+                    _phoneNumber = phoneNumberText;
+                  },
+                  inputBorder: OutlineInputBorder(),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [primaryColor, secondaryColor],
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 20,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sent OTP",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
